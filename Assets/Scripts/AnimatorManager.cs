e02edff94394ef9043b8183d3e464db658ac9c23
Krishna <krishna.tyagi.dev@gmail.com>
2026-09-06 23:16:43 +0530

    Add Basic 3D Player Movement And Animations

---
+++ Assets/Scripts/AnimatorManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    int vertical;
+    int horizontal;
+
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal");
+        vertical = Animator.StringToHash("Vertical");
+    }
+
+
+
+    public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+    {
+
+        float snappedHorizontal;
+        float snappedVertical;
+
+
+        #region  snapped Horizontal
+        if(horizontalMovement>0 && horizontalMovement < 0.55f)
+        {
+            snappedHorizontal = 0.5f;
+        }
+        else if(horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = 1;
+        }
+        else if(horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            snappedHorizontal = -0.5f;
+        }
+        else if(horizontalMovement < -0.55f)
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+
+
+
+        #region  snapped Vertical
+        if(verticalMovement>0 && verticalMovement < 0.55f)
+        {
+            snappedVertical = 0.5f;
+        }
+        else if(verticalMovement > 0.55f)
+        {
+            snappedVertical = 1;
+        }
+        else if(verticalMovement < 0 && verticalMovement > -0.55f)
+        {
+            snappedVertical = -0.5f;
+        }
+        else if(verticalMovement < -0.55f)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+
+       animator.SetFloat(horizontal, snappedHorizontal, .1f , Time.deltaTime);
+       animator.SetFloat(vertical, verticalMovement , .1f , Time.deltaTime);
+    }
+
+
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerControls playercontrols;
+    AnimatorManager animatorManager;
 
     public Vector2 movementInput;
+    private float moveAmount;
     public float verticalInput;
     public float horizontalInput;
 
+
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
 
     private void OnEnable()
     {
@@ -36,6 +44,8 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0,moveAmount);
     }
 
 
